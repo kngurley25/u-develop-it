46c5895a45085f7fed5082c6f1c6e98b70410905
kngurley <kngurley25@gmail.com>
2021-12-01 18:24:57 -0600

    modularize app

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+// central for all api routes
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+module.exports = router;
